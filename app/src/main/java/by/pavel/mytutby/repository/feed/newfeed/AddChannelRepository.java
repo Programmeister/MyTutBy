216eb36b0bb2f07d965c587c0a52dfb60e388015
Pavel <pavelkiselevich99@gmail.com>
2020-12-02 22:13:41 +0300

    feat: add feeds search

--- app/src/main/java/by/pavel/mytutby/repository/feed/newfeed/AddChannelRepository.java
+++ app/src/main/java/by/pavel/mytutby/repository/feed/newfeed/AddChannelRepository.java
@@ -1,8 +1,7 @@
 package by.pavel.mytutby.repository.feed.newfeed;
 
 import android.content.Context;
-
-import com.tickaroo.tikxml.TikXml;
+import android.util.Log;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -20,8 +19,6 @@ import javax.inject.Inject;
 
 import by.pavel.mytutby.R;
 import by.pavel.mytutby.data.Feed;
-import by.pavel.mytutby.data.feed.Html;
-import by.pavel.mytutby.data.feed.Link;
 import by.pavel.mytutby.network.FeedService;
 import by.pavel.mytutby.util.toaster.ToastProvider;
 import dagger.hilt.android.qualifiers.ApplicationContext;
@@ -32,41 +29,32 @@ public class AddChannelRepository implements NewFeedRepository {
 
     private final Context appContext;
     private final FeedService feedService;
-    private final ExecutorService executorService;
     private final ToastProvider toastProvider;
+    private final ExecutorService executorService;
 
     @Inject
-    public AddChannelRepository(@ApplicationContext Context context, FeedService service, ToastProvider toaster) {
+    public AddChannelRepository(@ApplicationContext Context context,
+                                FeedService service,
+                                ToastProvider toaster) {
         appContext = context;
         feedService = service;
-        executorService = Executors.newFixedThreadPool(1);
         toastProvider = toaster;
+        executorService = Executors.newFixedThreadPool(1);
     }
 
-    // TODO: Don't work
     @Override
     public List<Feed> getNewFeeds(String url) {
         Future<List<Feed>> future = executorService.submit(() -> {
             try {
                 Call<ResponseBody> call = feedService.getFeedsLinks(url);
                 ResponseBody body = call.execute().body();
-                if (body == null)
-                    return null;
-
-                String s = body.string();
-
-                TikXml parser = new TikXml.Builder().exceptionOnUnreadXml(false).build();
-                Html html = parser.read(body.source(), Html.class);
-                List<Link> list = html.links;
-                List<Feed> feeds = new ArrayList<>();
-                for (Link link : list)
-                    if (link.type.contains("application/rss"))
-                        feeds.add(new Feed(link.title, link.href));
-                return feeds;
+                if (body != null)
+                    return parseFeedsFromHtml(body.string());
             } catch (IOException e) {
+                e.printStackTrace();
                 toastProvider.showToast(R.string.connection_problem);
-                return null;
             }
+            return null;
         });
         try {
             return future.get();
@@ -81,16 +69,50 @@ public class AddChannelRepository implements NewFeedRepository {
         String fileName = appContext.getString(R.string.file_name);
         String separator = appContext.getString(R.string.separator);
         String line = feed.title + separator + feed.link;
+
+        if (!isLineRecorded(fileName, line))
+            writeLine(fileName, line);
+    }
+
+    private List<Feed> parseFeedsFromHtml(String htmlString) {
+        List<Feed> feeds = new ArrayList<>();
+
+        String[] strings = htmlString.substring(
+                htmlString.indexOf("<head"),
+                htmlString.indexOf("</head>")
+        ).split("<");
+
+        for (String s : strings) {
+            if (s.contains("link") && s.contains("rss")) {
+                int i1 = s.indexOf("title=") + 7;
+                int i2 = s.indexOf('"', i1);
+                String title = s.substring(i1, i2);
+                i1 = s.indexOf("href=") + 7;
+                i2 = s.indexOf('"', i1);
+                String href = s.substring(i1, i2);
+                feeds.add(new Feed(title, href));
+                Log.d("RESULT", title + " ## " + href);
+            }
+        }
+        return feeds;
+    }
+
+    private boolean isLineRecorded(String fileName, String line) {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(
                 appContext.openFileInput(fileName)
         ))) {
             String str;
             while ((str = reader.readLine()) != null)
                 if (line.equals(str))
-                    return;
+                    return true;
+            return false;
         } catch (IOException e) {
             e.printStackTrace();
+            return false;
         }
+    }
+
+    private void writeLine(String fileName, String line) {
         try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(
                 appContext.openFileOutput(fileName, Context.MODE_APPEND)
         ))) {
